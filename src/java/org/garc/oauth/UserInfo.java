0a404eeddf37aa425d77db348e01876a00ceb21e
Palani Ramkumar <palaniramkumar@gmail.com>
2014-09-20 16:45:28 +0530

    user object as json object

--- src/java/org/garc/oauth/UserInfo.java
+++ src/java/org/garc/oauth/UserInfo.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package org.garc.oauth;
 
 import java.io.IOException;
@@ -11,29 +10,49 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.garc.config.DBObject;
+import org.json.simple.JSONObject;
+
 /**
  *
  * @author Ramkumar
  */
 public class UserInfo {
-     public void getAuthToken(String username, String password) throws ClassNotFoundException, SQLException, IOException {
-        DBObject dbObj=new DBObject();
-        PreparedStatement stmt = dbObj.getDbConection().prepareStatement("select staff_id,user_type,DATE_FORMAT(day, '%W %d,%M %Y') ,DATE_FORMAT(day, '%r')  from staff where user_name = ? and pass=password(?)");
+
+    public JSONObject getAuthToken(String username, String password) throws ClassNotFoundException, SQLException, IOException {
+        DBObject dbObj = new DBObject();
+        JSONObject json = new JSONObject();
+        PreparedStatement stmt = dbObj.getDbConection().prepareStatement("select staff_id,qualification,designation,title,staff_name,user_name,user_type,DATE_FORMAT(day, '%W %d,%M %Y') ,DATE_FORMAT(day, '%r') last_login_at from staff where user_name = ? and pass=password(?)");
         stmt.setString(1, username);
         stmt.setString(2, password);
-        ResultSet result=stmt.executeQuery();
+        ResultSet result = stmt.executeQuery();
         if (result.next()) {
-           System.out.println(result.getString(1));
-        }
-        else{
+            json.put("id", result.getString("staff_id"));
+            json.put("name", result.getString("staff_name"));
+            json.put("type", result.getString("user_type"));
+            json.put("login_id", result.getString("user_name"));
+            json.put("qualification", result.getString("qualification"));
+            json.put("designation", result.getString("designation"));
+            json.put("title", result.getString("title"));
+            json.put("last_login_at", result.getString("last_login_at"));
+        } else {
             stmt.close();
-            stmt = dbObj.getDbConection().prepareStatement("select semester,section,student_id, DATE_FORMAT(day, '%W %d,%M %Y') ,DATE_FORMAT(day, '%r')  from students where username= ? and pass=password(?)");
+            stmt = dbObj.getDbConection().prepareStatement("select student_name,username,semester,section,student_id, DATE_FORMAT(day, '%W %d,%M %Y') ,DATE_FORMAT(day, '%r') last_login_at,isattend from students where username= ? and pass=password(?)");
             stmt.setString(1, username);
             stmt.setString(2, password);
-            result=stmt.executeQuery();
-            if(result.next()){
-                System.out.println(result.getString(1));
+            result = stmt.executeQuery();
+            if (result.next()) {
+                json.put("id", result.getString("student_id"));
+                json.put("name", result.getString("student_name"));
+                json.put("semester", result.getString("semester"));
+                json.put("section", result.getString("section"));
+                json.put("login_id", result.getString("username"));
+                json.put("type", "student");
+                json.put("block", result.getString("isattend"));
+                
+                json.put("last_login_at", result.getString("last_login_at"));
             }
         }
+        dbObj.finalize();
+        return json;
     }
 }
